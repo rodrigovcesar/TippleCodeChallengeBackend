34141498c59697fbb85e4c9214ff4ebfa6a1b511
Rodrigo Vasconcellos Cesar <vasconcellos.rodrigo@outlook.com>
2020-03-28 22:19:35 +1100

    First test for filtered cocktails list

---
+++ Tests/CocktailDBApiTests/ApiTests.cs
@@ -0,0 +1,34 @@
+using CocktailDBApi;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace CocktailDBApiTests
+{
+    public class ApiTests
+    {
+        private readonly ITestOutputHelper _output;
+        private DBApi _cocktailDBApi;
+
+        public ApiTests(ITestOutputHelper outputHelper)
+        {
+            _output = outputHelper;
+            _cocktailDBApi = new DBApi();
+
+        }
+        [Fact]
+        public async Task GetIngredientSearch()
+        {
+            var ingredient = "Gin";
+
+            var cocktails = await _cocktailDBApi.GetIngredientSearch(ingredient);
+
+
+            Assert.Equal(95, cocktails.Drinks.Count());
+        }
+
+        
+    }
+}
