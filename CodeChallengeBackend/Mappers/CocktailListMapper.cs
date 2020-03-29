8994123d9b8303a8d5628c6fb377730addb38dde
Rodrigo Vasconcellos Cesar <vasconcellos.rodrigo@outlook.com>
2020-03-29 11:07:53 +1100

    Separation of mappers for single cocktail and list

---
+++ CodeChallengeBackend/Mappers/CocktailListMapper.cs
@@ -0,0 +1,28 @@
+﻿using api.Models.Response;
+using AutoMapper;
+using Models;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace api.Mappers
+{
+    public class CocktailListMapper : Profile
+    {
+        public CocktailListMapper()
+        {
+            CreateMap<IEnumerable<CocktailResponse>, CocktailList>()
+           .AfterMap((src, dest, context) =>
+           {
+               dest.Cocktails = src.Select(s => context.Mapper.Map<Cocktail>(s.Drinks.First())).ToList();
+               dest.meta = new ListMeta()
+               {
+                   count = dest.Cocktails.Count,
+                   firstId = src.First().Drinks.First().IdDrink,
+                   lastId = src.Last().Drinks.First().IdDrink,
+                   medianIngredientCount = (int)dest.Cocktails.Average(c => c.Ingredients.Count)
+               };
+           });
+        }       
+    }
+}
--- CodeChallengeBackend/Mappers/CocktailMapper.cs
+++ CodeChallengeBackend/Mappers/CocktailMapper.cs
@@ -13,30 +13,28 @@ namespace api.Mappers
         public CocktailMapper()
         {
             RecognizePostfixes("Drink");
-            RecognizePrefixes("Str");           
+            RecognizePrefixes("Str");
 
             CreateMap<CocktailAllProperties, Cocktail>()
                 .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.StrDrink))
                 .ForMember(dest => dest.ImageURL, opt => opt.MapFrom(src => src.StrDrinkThumb))
                 .ForMember(dest => dest.Ingredients, opt => opt.MapFrom(src => MapIngredients(src)));
-
-            CreateMap<CocktailResponse, Cocktail>()
-            .AfterMap((src, dest, context) => context.Mapper.Map(src.Drinks.First(), dest));
+            
 
         }
 
         private List<string> MapIngredients(CocktailAllProperties source)
-        {                   
+        {
             var pattern = new Regex(@"^StrIngredient\d+$");
             var ingredientsList = source.GetType().
                 GetProperties()
                 .Where(p => pattern.IsMatch(p.Name) && !string.IsNullOrEmpty((string)p.GetValue(source)))
                 .Select(p => (string)p.GetValue(source))
-                .ToList(); 
+                .ToList();
 
             return ingredientsList;
         }
 
-        
+
     }
 }
